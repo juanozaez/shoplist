5dd57d16be6bf5d3c539f1a30f1b296e4ff4c199
Juanlu <juan.ozaez@wallapop.com>
2019-10-27 18:30:59 +0100

    Add delete view integration test

--- src/test/kotlin/com/home/shoplist/context/food/integration/persistence/JpaFoodViewRepositoryTest.kt
+++ src/test/kotlin/com/home/shoplist/context/food/integration/persistence/JpaFoodViewRepositoryTest.kt
@@ -3,6 +3,7 @@ package com.home.shoplist.context.food.integration.persistence
 import com.home.shoplist.context.food.domain.query.FoodView
 import com.home.shoplist.context.food.domain.query.FoodViewRepository
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import javax.inject.Inject
@@ -19,5 +20,15 @@ class JpaFoodViewRepositoryTest {
         jpaFoodViewRepository.save(foodView)
 
         assertEquals(foodView, jpaFoodViewRepository.find("1"))
+    }
+
+    @Test
+    fun `it should delete food view`() {
+        val foodView = FoodView("1", "Pear")
+        jpaFoodViewRepository.save(foodView)
+
+        jpaFoodViewRepository.delete("1")
+
+        assertNull(jpaFoodViewRepository.find("1"))
     }
 }
